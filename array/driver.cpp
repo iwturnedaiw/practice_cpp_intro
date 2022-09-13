0bd035b1afae17560b6aa185120fe28819fc15b9
Shohei Minami <minami.s.ae@m.titech.ac.jp>
2022-09-13 19:23:58 +0900

    update driver

--- array/driver.cpp
+++ array/driver.cpp
@@ -14,5 +14,11 @@ int main () {
   std::cout << a[3] << std::endl;
   std::cout << a[4] << std::endl;
 
+  const scratch::array<int, 3> b = {0, 1, 2};
+  std::cout << b[2] << std::endl;
+  std::cout << "a size: " << a.size() << std::endl;
+  std::cout << "b size: " << b.size() << std::endl;
+
+
   return 0;
 }
