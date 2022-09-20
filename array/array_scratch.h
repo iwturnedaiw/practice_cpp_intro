4d53fe0c52b0aa96d1f5f7845c0f8369dc2bb139
Shohei Minami <minami.s.ae@m.titech.ac.jp>
2022-09-21 04:49:45 +0900

    const_iterator

--- array/array_scratch.h
+++ array/array_scratch.h
@@ -30,7 +30,28 @@ namespace scratch {
       --*this;
       return copy; 
     }
+    array_iterator & operator += (std::size_t n) {
+      i += n;
+      return *this; 
+    }
+    array_iterator operator + (std::size_t n) const {
+      array_iterator copy = *this;
+      copy += n;
+      return copy; 
+    }
+    array_iterator & operator -= (std::size_t n) {
+      i -= n;
+      return *this; 
+    }
+    array_iterator operator - (std::size_t n) const {
+      array_iterator copy = *this;
+      copy -= n;
+      return copy; 
+    }
 
+    typename Array::reference operator [] (std::size_t n) const {
+      return *( *this + n) ;
+    }
 
     bool operator == (array_iterator const& right) {
       return i == right.i;
@@ -38,8 +59,95 @@ namespace scratch {
     bool operator != (array_iterator const& right) {
       return i != right.i;
     }
+
+    bool operator < (array_iterator const & right ) const {
+      return i < right.i;
+    }
+    bool operator <= (array_iterator const & right ) const {
+      return i <= right.i;
+    }
+    bool operator > (array_iterator const & right ) const {
+      return i > right.i;
+    }
+    bool operator >= (array_iterator const & right ) const {
+      return i >= right.i;
+    }
   };
 
+
+  template <typename Array >
+  struct array_const_iterator {
+    Array const & a;
+    std::size_t i;
+
+    array_const_iterator (Array const& a, std::size_t i) : a(a), i(i) { }
+    array_const_iterator(typename Array::iterator const & iter ): a( iter.a ), i( iter.i ) { }
+
+    typename Array::const_reference operator * () const {
+      return a[i];
+    }
+
+    array_const_iterator & operator ++ () {
+      ++i;
+      return *this;
+    }
+    array_const_iterator operator ++ (int) {
+      array_const_iterator copy = *this;
+      ++*this;
+      return copy;
+    }
+    array_const_iterator operator -- () {
+      --i;
+      return *this;
+    }
+    array_const_iterator operator -- (int) {
+      array_const_iterator copy = *this;
+      --*this;
+      return copy; 
+    }
+    array_const_iterator & operator += (std::size_t n) {
+      i += n;
+      return *this; 
+    }
+    array_const_iterator operator + (std::size_t n) const {
+      array_const_iterator copy = *this;
+      copy += n;
+      return copy; 
+    }
+    array_const_iterator & operator -= (std::size_t n) {
+      i -= n;
+      return *this; 
+    }
+    array_const_iterator operator - (std::size_t n) const {
+      array_const_iterator copy = *this;
+      copy -= n;
+      return copy; 
+    }
+
+    typename Array::const_reference operator [] (std::size_t i) const {
+      return *(*this+ i);
+    }
+
+    bool operator == (array_const_iterator const& right) {
+      return i == right.i;
+    }
+    bool operator != (array_const_iterator const& right) {
+      return i != right.i;
+    }
+
+    bool operator < (array_const_iterator const & right ) const {
+      return i < right.i;
+    }
+    bool operator <= (array_const_iterator const & right ) const {
+      return i <= right.i;
+    }
+    bool operator > (array_const_iterator const & right ) const {
+      return i > right.i;
+    }
+    bool operator >= (array_const_iterator const & right ) const {
+      return i >= right.i;
+    }
+  };
 
   template <typename T, std::size_t N>
   struct array {
@@ -48,6 +156,7 @@ namespace scratch {
     using const_reference = T const &;
     using size_type = std::size_t;
     using iterator = array_iterator<array>;
+    using const_iterator = array_const_iterator<array>;
 
     value_type storage[N];
 
@@ -74,6 +183,19 @@ namespace scratch {
     }
     iterator end() {
       return iterator(*this, N);
+    }
+
+    const_iterator begin() const {
+      return const_iterator(*this, 0);
+    }
+    const_iterator end() const {
+      return const_iterator(*this, N);
+    }
+    const_iterator cbegin() const {
+      return const_iterator(*this, 0);
+    }
+    const_iterator cend() const {
+      return const_iterator(*this, N);
     }
   };
 };
--- array/driver.cpp
+++ array/driver.cpp
@@ -8,18 +8,18 @@ int main () {
 //  scratch::array<int, 5> a;
   a[4] = 4;
 
-  std::cout << a[0] << std::endl;
-  std::cout << a[1] << std::endl;
-  std::cout << a[2] << std::endl;
-  std::cout << a[3] << std::endl;
-  std::cout << a[4] << std::endl;
+  std::cout << "a[0]: 0 == " << a[0] << std::endl;
+  std::cout << "a[1]: 1 == " << a[1] << std::endl;
+  std::cout << "a[2]: 2 == " << a[2] << std::endl;
+  std::cout << "a[3]: 3 == " << a[3] << std::endl;
+  std::cout << "a[4]: 4 == " << a[4] << std::endl;
 
   a.fill(100);
-  std::cout << a[0] << std::endl;
-  std::cout << a[1] << std::endl;
-  std::cout << a[2] << std::endl;
-  std::cout << a[3] << std::endl;
-  std::cout << a[4] << std::endl;
+  std::cout << "a[0]: 100 == " << a[0] << std::endl;
+  std::cout << "a[1]: 100 == " << a[1] << std::endl;
+  std::cout << "a[2]: 100 == " << a[2] << std::endl;
+  std::cout << "a[3]: 100 == " << a[3] << std::endl;
+  std::cout << "a[4]: 100 == " << a[4] << std::endl;
 
   const scratch::array<int, 3> b = {0, 1, 2};
   std::cout << b[2] << std::endl;
@@ -30,20 +30,78 @@ int main () {
   a[0] = 0; a[1] = 1; a[2] = 2; a[3] = 3; a[4] = 4;
 
   auto itr = a.begin();
-  std::cout << *(itr++) << std::endl;
-  std::cout << *itr << std::endl;
-  std::cout << *(++itr) << std::endl;
-  std::cout << *itr << std::endl;
+  std::cout << "*itr++: 0 == " << *(itr++) << std::endl;
+  std::cout << "*itr  : 1 == " << *itr << std::endl;
+  std::cout << "*++itr: 2 == " << *(++itr) << std::endl;
+  std::cout << "*itr  : 2 == " << *itr << std::endl;
 
   auto itr1 = a.end();
-  std::cout << *(--itr1) << std::endl;
-  std::cout << *(itr1--) << std::endl;
-  std::cout << *itr1 << std::endl;
+  std::cout << "*--itr: 4 == " << *(--itr1) << std::endl;
+  std::cout << "*itr--: 4 == " << *(itr1--) << std::endl;
+  std::cout << "*itr  : 3 == " << *itr1  << std::endl;
 
+  std::cout << "loop: " << std::endl;
   for (auto itr = a.begin(); itr != a.end(); ++itr) {
     std::cout << *itr << std::endl;
   }
-  
+
+
+  auto itr2 = a.begin();
+  std::cout << "*(itr+3): 3 == " << *(itr2 + 3) << std::endl;
+  std::cout << "itr[2]  : 2 == " << itr2[2] << std::endl;
+  auto itr3 = a.end();
+  std::cout << "*(itr-3): 1 == " << *(itr3 - 3) << std::endl;
+
+  auto b1 = (itr2 < itr3);
+  std::cout << "< : 1 == " << b1 << std::endl;
+  b1 = (itr2 > itr3);
+  std::cout << "> : 0 == " << b1 << std::endl;
+  itr2 += 5;
+  b1 = (itr2 < itr3);
+  std::cout << "< : 0 == " << b1 << std::endl;
+  b1 = (itr2 <= itr3);
+  std::cout << "<=: 1 == " << b1 << std::endl;
+  b1 = (itr3 > itr2);
+  std::cout << "> : 0 == " << b1 << std::endl;
+  b1 = (itr3 >= itr2);
+  std::cout << ">=: 1 == " << b1 << std::endl;
+
+  auto ci = a.cbegin();
+  //*ci = -100;
+  std::cout << "*itr++: 0 == " << *(ci++) << std::endl;
+  std::cout << "*itr  : 1 == " << *ci << std::endl;
+  std::cout << "*++itr: 2 == " << *(++ci) << std::endl;
+  std::cout << "*itr  : 2 == " << *ci << std::endl;
+
+  auto cie = a.cend();
+  std::cout << "*--itr: 4 == " << *(--cie) << std::endl;
+  std::cout << "*itr--: 4 == " << *(cie--) << std::endl;
+  std::cout << "*itr  : 3 == " << *cie  << std::endl;
+
+  std::cout << "loop: " << std::endl;
+  for (auto itr = a.cbegin(); itr != a.cend(); ++itr) {
+    std::cout << *itr << std::endl;
+  }
+
+  auto citr2 = a.cbegin();
+  std::cout << "*(itr+3): 3 == " << *(citr2 + 3) << std::endl;
+  std::cout << "itr[2]  : 2 == " << citr2[2] << std::endl;
+  auto citr3 = a.cend();
+  std::cout << "*(itr-3): 1 == " << *(citr3 - 3) << std::endl;
+
+  auto cb1 = (citr2 < citr3);
+  std::cout << "< : 1 == " << cb1 << std::endl;
+  cb1 = (citr2 > citr3);
+  std::cout << "> : 0 == " << cb1 << std::endl;
+  citr2 += 5;
+  cb1 = (citr2 < citr3);
+  std::cout << "< : 0 == " << cb1 << std::endl;
+  cb1 = (citr2 <= citr3);
+  std::cout << "<=: 1 == " << cb1 << std::endl;
+  cb1 = (citr3 > citr2);
+  std::cout << "> : 0 == " << cb1 << std::endl;
+  cb1 = (citr3 >= citr2);
+  std::cout << ">=: 1 == " << cb1 << std::endl;
 
   return 0;
 }
